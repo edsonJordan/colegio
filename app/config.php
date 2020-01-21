d029c6014bcfc5121eb67e72a3794f66b9fbbed1
Edson Jordan <edson_4555@hotmail.com>
2020-01-20 22:58:33 -0500

    configuracion general

--- app/config.php
+++ app/config.php
@@ -5,7 +5,14 @@ define('DB_PASSWORD', '');
 define('DB_NAME', 'db_transporte');
 
 define('RUTAL_APP', dirname(dirname(__FILE__)));
+/* RUTA DEL SISTEMA GENERAL */
 define('RUTA_URL', 'http://localhost:8080/colegio');
 define('NAME_SITE', 'Sistema de colegio');
+/* RUTA DE MODULOS */
 define('MODULO_MATRICULA', NAME_SITE.'-Matricula');
 define('MODULO_LIBRETA', NAME_SITE.'-Libretas');
+/* RUTA DE RECURSOS PUBLIC */
+define('RUTA_TIPOGRAFIA', RUTAL_APP.'tipografias/');
+define('RUTA_IMAGENES', RUTAL_APP.'img/');
+define('RUTA_ESTILOS', RUTAL_APP.'css/');
+define('RUTA_javascripts', RUTAL_APP.'js/');
