9897412cd9d8ccb4cb0d7f2caf178bffd07db96e
Edson Jordan <edson_4555@hotmail.com>
2020-01-20 22:55:26 -0500

    conguracion general

---
+++ app/config.php
@@ -0,0 +1,11 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'db_transporte');
+
+define('RUTAL_APP', dirname(dirname(__FILE__)));
+define('RUTA_URL', 'http://localhost:8080/colegio');
+define('NAME_SITE', 'Sistema de colegio');
+define('MODULO_MATRICULA', NAME_SITE.'-Matricula');
+define('MODULO_LIBRETA', NAME_SITE.'-Libretas');
